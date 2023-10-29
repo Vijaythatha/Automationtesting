2ee751d0010ca26dbc61b87cf4e0c96aa970eb85
Vijaythatha <thatha_vijaykrishna@yahoo.com>
2023-10-29 10:43:58 +0530

    adding new changes

--- GITUsingToke/src/GITusingToken.java
+++ GITUsingToke/src/GITusingToken.java
@@ -4,6 +4,8 @@ public class GITusingToken {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GIT using token");
+		System.out.println("hello");
+		System.out.println("Automation");
 
 	}
 
